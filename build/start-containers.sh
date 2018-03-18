1c1ce1ea57844996a4981abe98868ddea02615ef
Vaibhav Desai <vaidesai@paypal.com>
2018-03-18 02:55:22 -0700

    more typo fixes

--- build/start-containers.sh
+++ build/start-containers.sh
@@ -9,21 +9,21 @@ docker network rm atum-network
 
 # Pull images
 echo "\nPulling images..."
-docker pull vaibhavdesai137/atum-mysql:master-???
-docker pull vaibhavdesai137/atum-tomcat:master-???
+docker pull vaibhavdesai137/atum-mysql:master-28f31c
+docker pull vaibhavdesai137/atum-tomcat:master-28f31c
 
 # Create network and start containers
 echo "\nCreating new networks..."
 docker network create atum-network
 
 echo "\nStarting atum-mysql container..."
-docker run --net atum-network -p 3306:3306 --name atum-mysql -d vaibhavdesai137/atum-mysql:master-???
+docker run --net atum-network -p 3306:3306 --name atum-mysql -d vaibhavdesai137/atum-mysql:master-28f31c
 
-echo "\nSleeping for 15 seconds for mysql to be up..."
-sleep 15
+echo "\nSleeping for 30 seconds for mysql to be up..."
+sleep 30
 
 echo "\nStarting atum-tomcat container..."
-docker run --net atum-network -p 8080:8080 --name atum-tomcat -d vaibhavdesai137/atum-tomcat:master-???
+docker run --net atum-network -p 8080:8080 --name atum-tomcat -d vaibhavdesai137/atum-tomcat:master-28f31c
 
 echo "\nStarting atum-tomcat container..."
 docker ps
